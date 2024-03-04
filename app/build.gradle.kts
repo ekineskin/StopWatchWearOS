f34d7b0c0b039e3ea8be25fe3ef0b756c138bbf4
Ekin Eşkin <86964338+ekineskin@users.noreply.github.com>
2024-03-04 17:20:23 +0300

    StopWatchWearOS
    StopWatchWearOS is a stopwatch application developed for Android smartwatches using Kotlin and Jetpack Compose.

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.stopwatchwearos"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.stopwatchwearos"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.wear.compose:compose-material:1.3.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.2")
+    implementation("androidx.wear.compose:compose-foundation:1.3.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.wear.tiles:tiles:1.3.0")
+    implementation("androidx.wear.tiles:tiles-material:1.3.0")
+    implementation("com.google.android.horologist:horologist-compose-tools:0.4.8")
+    implementation("com.google.android.horologist:horologist-tiles:0.4.8")
+    implementation("androidx.wear.watchface:watchface-complications-data-source-ktx:1.2.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+}
---
+++ app/src/main/java/com/example/stopwatchwearos/presentation/TimerState.kt
@@ -0,0 +1,5 @@
+package com.example.stopwatchwearos.presentation
+
+enum class TimerState {
+    RUNNING, PAUSED, RESET
+}
